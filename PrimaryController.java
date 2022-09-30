bbcd67e12a9399c9d4bb188e1b5e1297e17b01d0
DonnyP-Augie <donaldpeters19@augustana.edu>
2022-09-30 18:15:39 -0500

    Commitment

---
+++ PrimaryController.java
@@ -0,0 +1,49 @@
+package edu.augustana.UIMockup;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+
+public class PrimaryController {
+
+	@FXML
+	private Button GenRandomButton;
+
+	@FXML
+	private Button loadFileButton;
+
+	@FXML
+	private Button scratchButton;
+
+	@FXML
+	private Button templateButton;
+
+	@FXML
+	void genRandom(ActionEvent event) throws IOException {
+		//Will do other stuff here too
+		
+		switchToThird(event);
+	}
+
+	@FXML
+	void loadExistingFile(ActionEvent event) {
+		FileChooser file = new FileChooser();
+		
+	}
+
+	@FXML
+	private void switchToSecondary() throws IOException {
+		App.setRoot("secondary");
+	}
+
+	@FXML
+	void switchToThird(ActionEvent event) throws IOException {
+		App.setRoot("third");
+	}
+
+}
+
+
---
+++ SecondaryController.java
@@ -0,0 +1,47 @@
+
+
+package edu.augustana.UIMockup;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.image.ImageView;
+
+
+public class SecondaryController {
+
+    
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label mapNameLabel;
+
+    @FXML
+    private ScrollBar mapScrollBar;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private ImageView previewImageView;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    void clickNext(ActionEvent event) throws IOException {
+    	App.setRoot("third");
+    }
+
+    @FXML
+    void clickedBack(ActionEvent event) throws IOException {
+    	App.setRoot("primary");
+    }
+
+}
