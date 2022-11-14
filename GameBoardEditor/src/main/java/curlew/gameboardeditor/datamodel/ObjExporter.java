7fcea271e87ba55e93df71f8714e592a299c3dd1
farah <farahkhanbrinto21@augustana.edu>
2022-11-14 16:22:10 -0600

    Exports Obj

---
+++ GameBoardEditor/src/main/java/curlew/gameboardeditor/datamodel/ObjExporter.java
@@ -0,0 +1,76 @@
+package curlew.gameboardeditor.datamodel;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjExporter {
+	
+	TerrainMap map;
+	
+	private static int TOTAL_VERTICES = 8;
+	
+	private static int TOTAL_FACES = 6;
+	
+	private static int numOfTiles;
+	
+
+	public ObjExporter(TerrainMap map) {
+		super();
+		this.map = map;
+	}
+
+	
+	public void export(File objFile) throws IOException {
+		FileWriter fWriter = new FileWriter(objFile);
+		PrintWriter fileOut = new PrintWriter(fWriter);
+		exportVertices(fileOut);
+		exportFaces(fileOut);
+		fileOut.close();
+	}
+	private void exportVertices(PrintWriter fileOut) {
+		
+		for (int r = 0; r < map.getRows() ; r++) {
+			for ( int c = 0 ; c < map.getColumns() ; c++) {
+				double z = map.getHeight(r, c);
+				fileOut.println("v " + c      + " "  + r     + " "  + z);
+				fileOut.println("v " + c      + " "  + r     + " "  + 0);
+				fileOut.println("v " + c      + " "  + (r+1) + " "  + 0);
+				fileOut.println("v " + c      + " "  + (r+1) + " "  + z);
+				fileOut.println("v " + (c+1)  + " "  + r     + " "  + z);
+				fileOut.println("v " + (c+1)  + " "  + r     + " "  + 0);
+				fileOut.println("v " + (c+1)  + " "  + (r+1) + " "  + 0);
+				fileOut.println("v " + (c+1)  + " "  + (r+1) + " "  + z);
+				fileOut.println(" ");
+
+
+				}
+				
+			}
+		}
+	
+	
+	private void exportFaces(PrintWriter fileOut) {
+		int count = 0;
+		for (int r = 0; r < map.getRows() ; r++) {
+			for ( int c = 0 ; c < map.getColumns() ; c++) {
+				double z = map.getHeight(r, c);
+				int N = count++;
+				System.out.println("N = " + N);
+				fileOut.println("f " + (8*N+4) + " " + (8*N+3) + " " + (8*N+2) + " " + (8*N+1));
+				fileOut.println("f " + (8*N+2) + " " + (8*N+6) + " " + (8*N+5) + " " + (8*N+1));
+				fileOut.println("f " + (8*N+3) + " " + (8*N+7) + " " + (8*N+6) + " " + (8*N+2));
+				fileOut.println("f " + (8*N+8) + " " + (8*N+7) + " " + (8*N+3) + " " + (8*N+4));
+				fileOut.println("f " + (8*N+5) + " " + (8*N+8) + " " + (8*N+4) + " " + (8*N+1));
+				fileOut.println("f " + (8*N+6) + " " + (8*N+7) + " " + (8*N+8) + " " + (8*N+5));
+				fileOut.println(" ");
+
+			}
+		}
+		
+	}
+}
+
--- GameBoardEditor/src/main/java/curlew/gameboardeditor/datamodel/TestClass.java
+++ GameBoardEditor/src/main/java/curlew/gameboardeditor/datamodel/TestClass.java
@@ -1,25 +1,35 @@
 package curlew.gameboardeditor.datamodel;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestClass {
 
-	public static void main(String[] args) {
-		TerrainMap ourMap = new TerrainMap(10,10);
-		LandformsGenerator test = new GateToHellGenerator(ourMap);
-		test.build(1, 5, 3);
+	public static void main(String[] args) throws IOException {
+		TerrainMap ourMap = new TerrainMap(2,3);
+		ourMap = GameBoardIO.loadMap(new File("ACE.TMap"));
+		
+//		ourMap.build(0, 0);
+//		LandformsGenerator test = new GateToHellGenerator(ourMap);
+//		test.build(1, 5, 3);
 //		test.build(0,0,1);
 //		RandomMapGenerator g = new RandomMapGenerator(ourMap);
 //		g.createMap();
+		ObjExporter map = new ObjExporter(ourMap);
+		map.export(new File("test.obj"));
 		printMap(ourMap);
+		
 	}
 
-	public static void printMap(TerrainMap ourMap) {
-		for (int i = 0; i < ourMap.getRows(); i++) {
+	public static void printMap(TerrainMap map) {
+		for (int i = 0; i < map.getRows(); i++) {
 			System.out.println();
 
-			for (int j = 0; j < ourMap.getColumns(); j++) {
-				System.out.print(Math.round(ourMap.getHeight(i, j)) + " ");
-
+			for (int j = 0; j < map.getColumns(); j++) {
+				System.out.print(Math.round(map.getHeight(i, j)) + " ");
+				
 			}
+			
 		}
 
 	}
